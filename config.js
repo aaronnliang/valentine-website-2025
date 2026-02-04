2d1ca7051c60f34e10ec210058343fdd89a4b04a
aaronnliang <aaron.haohao@gmail.com>
2026-02-03 23:14:43 -0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://soundcloud.com/danielcaesar/toronto-2014", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dojfyeck0/video/upload/v1770189203/Daniel_Caesar_-_Toronto_2014_Official_Audio_gvj9za.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
